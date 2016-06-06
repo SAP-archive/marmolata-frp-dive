8f527a16c38ae98531d98a72df1ee0f44cb00a2c
Fabian Schmitthenner <fabian.schmitthenner@sap.com>
2016-06-07 01:36:48 +0200

    remove a TODO comment, since we basically decided that we won't do that anymore; add lots of ScalaDoc instead

--- core/shared/src/main/scala/ReactiveLibrary.scala
+++ core/shared/src/main/scala/ReactiveLibrary.scala
@@ -7,22 +7,19 @@ import cats._
 
 import scala.language.higherKinds
 
-// TODO: think about if we want to introduce implicits for ownership
-// to get rid of events/signals when they aren't used anymore
-// this may be particularly important in flatMap where a new event/signal
-// is created whenever a value changes and thus we may want to clean up
-// earlier signals/events soonish
-// if we still want to support for-notation, we probably need to do some macro-voodoo to
-// introduce implicit parameters in the right places, because Scala doesn't support automatically inserted
-// implicits in functions depending on their type signature
-// curiously, functions don't seem to support implicits in any way, only methods do and you can't even write a type signature for them
-
 object ReactiveLibrary {
   trait VarCompanionObject[Var[_]] {
+    /**
+      * returns a new assignable variable with initial value init
+      */
     def apply[A](init: A): Var[A]
   }
 
   trait SignalCompanionObject[Signal[_]] {
+    /**
+     * returns a constant signal that never changes its initial value
+     * this is the same as [[cats.Applicative#pure]]
+     */
     def Const[A](value: A): Signal[A]
   }
 
@@ -35,13 +32,42 @@ object ReactiveLibrary {
   }
 
   trait Observable[+A] {
+    /**
+      * observe the associated signal or event
+      * This has slightly different semantics with Signals and Events:
+      *
+      * == Usage as Signal ==
+      * When used on a a signal, the function f is triggered whenever the signal changes its value incuding once
+      * when observe is called. f is only ever called when the value changes afterwards.
+      *
+      * If the method shouldn't be called directly, but only when it first changes its value, use [[ReactiveLibrary#Signal#toEvent.observe]]
+      *
+      * == Usage as Event ==
+      * When used on an event, observe is called each time the event triggers. Thus it's not called directly after observe is called.
+      * Also, differently form the Signal case, it can be called with the same value twice in succession.
+      *
+      * == Additional information ==
+      * Besides future compositon methods like [[ReactiveLibraryUsage#SignalFutureExtensions#executeFuture]] this is
+      * the most important method to create side effects. It's especially not allowed to use [[Functor#map]]
+      * to do side effects. Instead, use this method
+      *
+      * @param f the method to call
+      * @return An object with which you can stop listening to the underlying Signal/event. When you call
+      */
     def observe(f: A => Unit): Cancelable
   }
 
   trait SignalTrait[+A] extends Observable[A] {
+    /**
+      * returns the current value of this Signal.
+      * It's generally better to use composition methods like [[Functor#map]], [[Cartesian#product]],
+      * [[ReactiveLibraryUsage#SignalExtensions#triggerWhen]] and similar methods.
+      * It should also be avoided to use now inside of any of these methods since the current value
+      * of a signal could be in an inconsistent state
+     */
     def now: A
 
-    @deprecated("TIMTOWTDIBSCINABTE: if you can, please use now, if you can't think twice", "0.32")
+    @deprecated("use now instead", "0.32")
     final def get: A = now
   }
 
@@ -51,8 +77,17 @@ object ReactiveLibrary {
   trait SignalOperationsTrait[F[+_]] extends Applicative[F]
 
   trait VarTrait[A] extends SignalTrait[A] {
+    /**
+      * update the value of this Var. This potentially triggers
+      * value changes of dependent variables and events
+      * and can also trigger side effects when any of those
+      * have an attached [[Observable#observe]] method.
+      *
+      * Note, that this is a noop when newValue is the old value of this variable
+      */
     def update(newValue: A): Unit
 
+    /** alias for update to use in infix notation */
     @inline
     final def := (newValue: A): Unit = update(newValue)
   }
@@ -65,6 +100,7 @@ object ReactiveLibrary {
   }
 
   trait Cancelable {
+    /** stop calling the associated function of the observe method that returned this object */
     def kill(): Unit
   }
 }
@@ -78,9 +114,19 @@ trait ReactiveLibrary {
   // see also http://stackoverflow.com/questions/37493183/how-to-make-scala-type-volatile-on-purpose
   protected type VolatileHelper
 
+  /**
+    * one of the tow most important types of this library besides [[Signal]]
+    */
   type Event[+A] <: EventTrait[A] with VolatileHelper
+
+  /**
+    * a time varying value and the other most important type of this library besides [[Event]]
+    */
   type Signal[+A] <: SignalTrait[A] with VolatileHelper
 
+  /** a time varying value that can be changed (and is generally only changed)
+    * when it's [[VarTrait#update]] method is called
+    */
   type Var[A] <: Signal[A] with VarTrait[A]
   type EventSource[A] <: Event[A] with EventSourceTrait[A]
 
