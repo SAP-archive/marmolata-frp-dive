2135b83c3eeb307e9b14a5c88d805fc5a5b71fd2
Fabian Schmitthenner <fabian.schmitthenner@sap.com>
2016-06-10 18:08:20 +0200

    change Test names (#9)

--- implementations/scalarx/shared/src/test/scala/Test.scala
+++ implementations/scalarx/shared/src/test/scala/Test.scala
@@ -2,6 +2,6 @@ import org.scalatest.FlatSpec
 import react.{ReactiveLibrary, ReactiveLibraryUsage}
 import react.Rx.TestImplementation
 
-class Test extends FlatSpec with TestImplementation {
+class ScalaRxTest extends FlatSpec with TestImplementation {
    def reactLibrary_ = reactive.library
 }
