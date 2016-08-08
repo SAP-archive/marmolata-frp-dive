0e90256987c57fafcf159c3129f200f060c6bc58
Fabian Schmitthenner <fabian.schmitthenner@sap.com>
2016-08-08 14:40:15 +0200

    fix selfrx test compilation

--- implementations/selfrx/shared/src/test/scala/SelfRxTest.scala
+++ implementations/selfrx/shared/src/test/scala/SelfRxTest.scala
@@ -1,11 +1,13 @@
+package react.impls.selfrx.tests
+
 import org.scalatest.{Matchers, FlatSpec}
-import react.LibTests.{ReactLibraryTests, TestConfiguration}
-import react.Rx.TestImplementation
-import react.core.ReactiveDeclaration
+import react.core.tests.{ReactLibraryTests, TestConfiguration,TestImplementation}
+import react.core.{ReactiveObject, ReactiveDeclaration}
 import react.impls.selfrx.SelfRxImpl
 
 class SelfRxTest extends FlatSpec with TestImplementation {
-  lazy val reactLibrary_ = new SelfRxImpl with ReactiveDeclaration
+  private lazy val reactive: ReactiveObject = new react.impls.selfrx.ReactiveObject()
+  lazy val reactLibrary_ = reactive.library
   override def shouldRunPropertyTests: Boolean = true
 
   override def testConfiguration: TestConfiguration =
