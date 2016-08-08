c2194aa6f03cf4303067b93db9b87cdc671a8d8b
Fabian Schmitthenner <fabian.schmitthenner@sap.com>
2016-08-08 16:12:45 +0200

    update metarx test definition

--- implementations/metarx/shared/src/test/scala/react/impls/metarx/tests/Test.scala
+++ implementations/metarx/shared/src/test/scala/react/impls/metarx/tests/Test.scala
@@ -3,6 +3,5 @@ package react.impls.metarx.tests
 import react.core.tests.DefaultTests
 
 class MetaRxTest extends DefaultTests {
-  def reactLibrary_ = reactive.library
-  override def shouldRunPropertyTests: Boolean = false
+  def reactiveLibrary_ = reactive.library
 }
